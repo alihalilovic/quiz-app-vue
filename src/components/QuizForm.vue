334a38bf321537907dea16b2904acc3b2bc7bd0d
alihalilovic <halilovicali03@gmail.com>
2022-05-11 16:22:37 +0200

    fixed mobile view on form

--- src/components/QuizForm.vue
+++ src/components/QuizForm.vue
@@ -54,17 +54,18 @@ const handleSubmitQuestionBlock = () => {
   width: 100vw;
   height: 100vh;
   position: absolute;
-  min-height:1500px;
+  min-height:1000px;
   left: 0;
   top: 0;
   z-index: 999;
 
   display: flex;
-  align-items: center;
+  align-items: flex-start;
   justify-content: center;
 }
 
 form {
+  margin-top: 1rem;
   display: flex;
   flex-direction: column;
   max-width: 550px;
